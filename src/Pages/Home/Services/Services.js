4fe2e4c5630a48a99f599aa5862e307ef459bf5e
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-11 02:11:01 +0600

    This project commponents add

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <p>this is services page</p>
+        </div>
+    );
+};
+
+export default Services;
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small>Copyright Protection &copy;{new Date().getFullYear()}</small></p>
+        </div>
+    );
+};
+
+export default Footer;
