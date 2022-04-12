56f3b5dbb99b0376d833048727cddcae9d26dfa3
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-13 04:27:21 +0600

    page reload solved

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import logo from "../../../images/logo.png";
 
 const Header = () => {
@@ -13,31 +14,31 @@ const Header = () => {
         sticky="top"
       >
         <Container>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand as={Link} to="/">
             {<img height={30} src={logo} alt="" />}
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link as={Link} to="/features">Features</Nav.Link>
+              <Nav.Link as={Link} to="/pricing">Pricing</Nav.Link>
               <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
+                <NavDropdown.Item as={Link} to="/action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/action/3.2">
                   Another action
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
+                <NavDropdown.Item as={Link} to="/action/3.3">
                   Something
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
+                <NavDropdown.Item as={Link} to="/action/3.4">
                   Separated link
                 </NavDropdown.Item>
               </NavDropdown>
             </Nav>
             <Nav>
-              <Nav.Link href="/about">about</Nav.Link>
-              <Nav.Link eventKey={2} href="/login">
+              <Nav.Link as={Link} to="/about">about</Nav.Link>
+              <Nav.Link eventKey={2} as={Link} to="/login">
                 Log in
               </Nav.Link>
             </Nav>
