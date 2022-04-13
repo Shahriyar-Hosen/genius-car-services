b0f7c3bd955e561759adc04c2a729558bd064fa5
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-13 17:46:52 +0600

    RequireAuth set up

---
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation } from 'react-router-dom';
+import auth from '../../../Firebase/Firebase.inite';
+
+const RequireAuth = ({ children }) => {
+    const [user] = useAuthState(auth);
+    let location = useLocation();
+
+    if (!user) {
+        return <Navigate to="/login" state={{ from: location }} replace />;   
+    }
+    return children
+};
+
+export default RequireAuth;
