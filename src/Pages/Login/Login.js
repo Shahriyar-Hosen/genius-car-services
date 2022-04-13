1f5ec3c27f0ca7962dc50dd82ebd37ae27fff137
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-13 15:45:48 +0600

    Style Login form use useRef to access input value

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useRef } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>This is Login page</h2>
-        </div>
-    );
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/register");
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(email, password);
+  };
+  return (
+    <div className="w-50 mx-auto">
+      <h1 className="text-primary text-center mt-2">Please Login</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <p className=" text-center mt-2">
+        Now to Genius car?{" "}
+        <span onClick={handleNavigate} className="text-danger">
+          Please Register
+        </span>
+      </p>
+    </div>
+  );
 };
 
 export default Login;
