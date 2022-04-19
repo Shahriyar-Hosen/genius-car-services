7924e022f1110099800dd9dbafa939800a6b0977
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-19 15:06:04 +0600

    Page Title change by route

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PageTitle from "../../Shared/PageTitle/PageTitle";
 import Experts from "../Experts/Experts";
 import Services from "../Services/Services";
 import Slider from "../Slider/Slider";
@@ -6,6 +7,7 @@ import Slider from "../Slider/Slider";
 const Home = () => {
   return (
     <div>
+      <PageTitle title="Home"></PageTitle>
       <Slider></Slider>
       <Services></Services>
       <Experts></Experts>
--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <HelmetProvider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </HelmetProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
