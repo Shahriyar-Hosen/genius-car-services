407f628f35667e9a1967edc54b50791b8ad55fdf
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-23 01:32:45 +0600

    Manage Services compo add & route setup

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./Pages/Home/Home/Home";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Login/Register/Register";
 import RequireAuth from "./Pages/Login/RequireAuth/RequireAuth";
+import ManageServices from "./Pages/ManageServices/ManageServices";
 import ServiceDetail from "./Pages/ServiceDetail/ServiceDetail";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Header from "./Pages/Shared/Header/Header";
@@ -28,7 +29,22 @@ function App() {
             </RequireAuth>
           }
         ></Route>
-        <Route path="/service/add" element={<AddService />}></Route>
+        <Route
+          path="/service/add"
+          element={
+            <RequireAuth>
+              <AddService />
+            </RequireAuth>
+          }
+        ></Route>
+        <Route
+          path="/service/manage"
+          element={
+            <RequireAuth>
+              <ManageServices />
+            </RequireAuth>
+          }
+        ></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/about" element={<About />}></Route>
---
+++ src/Pages/ManageServices/ManageServices.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ManageServices = () => {
+  return (
+    <div>
+      <h1>This service manege page</h1>
+    </div>
+  );
+};
+
+export default ManageServices;
