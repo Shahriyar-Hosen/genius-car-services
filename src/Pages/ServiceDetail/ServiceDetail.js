0fca855e9b5f86a9a1cab43a44a52820eadb53ec
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-22 16:24:26 +0600

    ServiceDetail data load with service id & display service name

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import PageTitle from "../Shared/PageTitle/PageTitle";
 
 const ServiceDetail = () => {
   const { serviceId } = useParams();
+  const [service, setService] = useState([]);
+  useEffect(() => {
+    const url = `http://localhost:5000/service/${serviceId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setService(data));
+  }, [serviceId]);
+
   return (
     <div>
-    <PageTitle title="Service Detail"></PageTitle>
-      <h1>This is service detail page:{serviceId}</h1>
+      <PageTitle title="Service Detail"></PageTitle>
+      <h1>This is service detail page:{service.name}</h1>
       <div className="text-center mt-3">
         <Link to="/checkout">
           <button className="btn btn-primary">Proceed Checkout</button>
