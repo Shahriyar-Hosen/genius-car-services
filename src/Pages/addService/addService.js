4347d423ca61257dc27ba35c7f26c4f9bff3e709
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-22 16:56:50 +0600

    react form setup

--- src/Pages/addService/addService.js
+++ src/Pages/addService/addService.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const AddService = () => {
-    return (
-        <div>
-            <h1>This add service page</h1>
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test" {...register("example")} />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("exampleRequired", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default AddService;
