68ac796f6da0628e84e9efdd8caab69a9b66195d
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-23 01:46:49 +0600

    use coustom hooks & load servide data

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,15 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import useServices from "../../hooks/useServices";
 import Service from "../Service/Service";
 import "./Services.css";
 
 const Services = () => {
-  const [services, setServices] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/service")
-      .then((res) => res.json())
-      .then((data) => setServices(data));
-  }, []);
+  const [services] = useServices([]);
 
   return (
     <div id="services" className="container">
--- src/Pages/ManageServices/ManageServices.js
+++ src/Pages/ManageServices/ManageServices.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const ManageServices = () => {
+    
   return (
     <div>
       <h1>This service manege page</h1>
