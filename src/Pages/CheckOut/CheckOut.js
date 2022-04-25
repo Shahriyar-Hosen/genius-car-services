88eb1d851ce39ad7b6455b90f65c8d7ec49606d9
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-26 02:14:28 +0600

    Order data sent in db

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -1,5 +1,7 @@
+// import axios from "axios";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 import auth from "../../Firebase/Firebase.inite";
 import useServiceDetails from "../hooks/useServiceDetails";
 import PageTitle from "../Shared/PageTitle/PageTitle";
@@ -8,11 +10,43 @@ const CheckOut = () => {
   const { serviceId } = useParams();
   const [service] = useServiceDetails(serviceId);
   const [user] = useAuthState(auth);
+
+  const handlePleaseOrder = (event) => {
+    event.preventDefault();
+    const order = {
+      email: user.email,
+      service: service.name,
+      serviceId: serviceId,
+      address: event.target.address.value,
+      phone: event.target.phone.value,
+    };
+
+    // axios.post("http://localhost:5000/order", order)
+    // .then((response) => {
+    //   console.log(response);
+    // });
+
+    fetch("http://localhost:5000/order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(order),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        event.target.reset();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
   return (
     <div className="container text-center">
       <PageTitle title="Order CheckOut"></PageTitle>
       <h1 className="text-primary my-5">Please Order: {service.name}</h1>
-      <form action="">
+      <form onSubmit={handlePleaseOrder}>
         <input
           className="my-1 w-100"
           type="text"
@@ -51,6 +85,7 @@ const CheckOut = () => {
           type="text"
           name="address"
           placeholder="address"
+          autoComplete="of"
           id=""
         />
         <br />
