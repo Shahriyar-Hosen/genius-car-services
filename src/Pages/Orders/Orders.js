ff9c4526acecf1c6fe3d1f74155068d82064b791
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-26 16:26:17 +0600

    authorization headers pass

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,17 +1,31 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../Firebase/Firebase.inite";
 
 const Orders = () => {
-    const [orders, setOrders] = useState([])
-    const [user] = useAuthState(auth);
-    useEffect(() => {
-        const email = user?.email;
-        const url = `http://localhost:5000/order/?email=${email}`;
-        fetch(url)
-          .then((res) => res.json())
-          .then((data) => setOrders(data));
-      }, [user]);
+  const [orders, setOrders] = useState([]);
+  const [user] = useAuthState(auth);
+  useEffect(() => {
+    const getOrders = async () => {
+      const email = user?.email;
+      const url = `http://localhost:5000/order/?email=${email}`;
+
+      const { data } = await axios.get(url, {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+
+      setOrders(data );
+    };
+
+    getOrders();
+
+    // fetch(url)
+    //   .then((res) => res.json())
+    //   .then((data) => setOrders(data));
+  }, [user]);
   return (
     <div>
       <h1>Your Orders: {orders?.length}</h1>
