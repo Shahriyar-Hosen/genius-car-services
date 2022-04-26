ef6f2177d263b844aa0719c9d1de7a275ec76aaf
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-26 18:06:06 +0600

    Handle 401, 403 to log out user

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,23 +1,33 @@
 import axios from "axios";
+import { signOut } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 import auth from "../../Firebase/Firebase.inite";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
   const [user] = useAuthState(auth);
+  const navigate = useNavigate();
   useEffect(() => {
     const getOrders = async () => {
       const email = user?.email;
       const url = `http://localhost:5000/order/?email=${email}`;
 
-      const { data } = await axios.get(url, {
-        headers: {
-          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-      });
-
-      setOrders(data );
+      try {
+        const { data } = await axios.get(url, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        });
+        setOrders(data);
+      } catch (error) {
+        console.log(error);
+        if (error.response.status === 401 || error.response.status === 403) {
+          signOut(auth);
+          navigate("/login");
+        }
+      }
     };
 
     getOrders();
